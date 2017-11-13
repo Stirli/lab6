99363ef91b8f02bf7b70a241c0322bc9dd030fa5
Дмитрий Владимирович Станский <d.v.stanski@outlook.com>
2017-11-13 23:12:59 +0300

    fix1

--- Lab6/Task4/Catalog.cs
+++ Lab6/Task4/Catalog.cs
@@ -11,18 +11,23 @@ namespace Lab6.Task4
         // Т.е. мы емеем свзяь многие-ко-многим. И искать нам нужно как по песням (которые могут дублироваться на несколько дисков),
         // так и по дискам. Поэтому делаем подобие реляционной бд на основе Hashtable. 
         // В качестве ключа будет использоваться строка, содержащая название диска. В качестве значения - список песен на этом диске.
-        private readonly Hashtable _disks = new Hashtable();
+        private readonly Hashtable _disks;
 
-        // Ключ - объект Song, значение - список дисков.
-        private readonly Hashtable _songs = new Hashtable();
+        // Список песен
+        private readonly ICollection<Song> _songs;
 
+        public Catalog()
+        {
+            _disks = new Hashtable();
+            _songs = new List<Song>();
+        }
         #region Disk
 
         // Добавляет диск в каталог и возвращает id диска.
         // Бросает ArgumentNullException
         public void AddDisk(string name)
         {
-            if(string.IsNullOrEmpty(name))
+            if (string.IsNullOrEmpty(name))
                 throw new ArgumentException("Имя диска не может быть пустым");
             if (_disks.Contains(name))
                 throw new ArgumentException("Диск уже существует");
@@ -32,12 +37,6 @@ namespace Lab6.Task4
         // Удалить диск.
         public void RemoveDisk(string name)
         {
-            // Удаляем диск из списка дисков для песен на этом диске
-            IEnumerable<Song> songs = EnumerateDisk(name);
-            foreach (Song song in songs)
-            {
-                (_songs[song] as ICollection<string>).Remove(name);
-            }
             // Удаление диска не приведет к удалению песен из каталога, если они были только на этом диске.
             _disks.Remove(name);
         }
@@ -58,32 +57,27 @@ namespace Lab6.Task4
 
             // Получаем список песен на диске.
             ICollection<Song> diskSongs = _disks[disk] as ICollection<Song>;
-            // Список дисков, на которых есть песня
-            ICollection<string> songDisks = _songs[song] as ICollection<string>;
 
             // Если песени нет на диске
-            if (diskSongs.Contains(song) || songDisks.Contains(disk))
+            if (diskSongs.Contains(song))
             {
                 throw new ArgumentException("Песня" + song + " уже добавлена на этот диск " + disk, "song");
             }
 
             // Добавляем песню на диск
             diskSongs.Add(song);
-            // Диск, в список дисков, на которых есть эта песня
-            songDisks.Add(disk);
         }
 
         public IEnumerable<string> EnumerateDisks()
         {
             return _disks.Keys.Cast<string>();
         }
+
         // Список песен на диске
         public IEnumerable<Song> EnumerateDisk(string disk)
         {
-            if (string.IsNullOrEmpty(disk))
-                throw new ArgumentException("Пустое имя диска");
             // _disks[disk] вернет object, который мы приводим к IEnumerable<Song>. если это значение будет null, будет брошено исключение
-            return _disks[disk] as IEnumerable<Song> ?? throw new ArgumentException("Диск не найден");
+            return _disks[disk] as IEnumerable<Song> ?? throw new ArgumentException("Диск '" + disk + "' не найден", "disk");
         }
 
         #endregion
@@ -96,7 +90,7 @@ namespace Lab6.Task4
         {
             if (_songs.Contains(song))
                 throw new ArgumentException("Песня уже существует", "song");
-            _songs.Add(song, new HashSet<string>());
+            _songs.Add(song);
         }
 
         // Удаляет песню из каталога. cascade - песня будет удалена со всех дисков.
@@ -108,10 +102,10 @@ namespace Lab6.Task4
                 return;
             }
 
-            var lists = _disks.Values.Cast<HashSet<Song>>();
-            foreach (var list in lists)
+            IEnumerable<ICollection<Song>> lists = _disks.Values.Cast<ICollection<Song>>();
+            foreach (ICollection<Song> list in lists)
             {
-                list.RemoveWhere(s => s.Equals(song));
+                list.Remove(song);
             }
         }
 
@@ -119,12 +113,12 @@ namespace Lab6.Task4
         // Список песен в каталоге
         public IEnumerable<Song> EnumerateCatalog()
         {
-            return _songs.Keys.Cast<Song>();
+            return _songs;
         }
 
         public IEnumerable<Song> FindSongs(string artistName)
         {
-            return EnumerateCatalog().Where(song => song.Artist.Equals(artistName));
+            return _songs.Where(song => song.Artist.Equals(artistName));
         }
         #endregion
     }
